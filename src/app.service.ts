18d4a1465729e0a26dea76b1957ea0627ee70945
PaulinaCM7 <90063591+PaulinaCM7@users.noreply.github.com>
2025-03-31 20:31:01 -0600

    Solución retornando un JSON con la correcta estructura
    - Se usa filter para obtener los personajes vivos
    - Se usa map y replace para modificar el nombre
    TODO:
    + Manejar el control de errores
    + Hacer pruebas

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,12 +12,14 @@ export class AppService {
       .pipe(
         map((response) => response.data),
         map((data) => ({
-          results: data.results.map((character) => ({
-            id: character.id,
-            name: character.name,
-            status: character.status,
-            gender: character.gender,
-          })),
+          results: data.results
+            .filter((character) => character.status === 'Alive')
+            .map((character) => ({
+              id: character.id,
+              name: character.name.replace(/\s/g, '_'),
+              status: character.status,
+              gender: character.gender,
+            })),
         }))
       );
   }
