3d48779ad68519a722e931ec3339e43314937342
PaulinaCM7 <90063591+PaulinaCM7@users.noreply.github.com>
2025-03-31 20:12:00 -0600

    Se obtiene la informacion de los personajes
    - Se obtiene la información de todos los personajes
    TODO:
    + Filtrar la informacion para los personajes que su estatus sea vivo
    + Modificar el nombre a guiones bajos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getCharacters() {
+    const characters = await this.appService.getCharacters();
+    return characters;
   }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  getCharacters() {
+    return this.httpService
+      .get('https://rickandmortyapi.com/api/character')
+      .pipe(
+        map((response) => response.data),
+        map((data) => ({
+          results: data.results.map((character) => ({
+            id: character.id,
+            name: character.name,
+            status: character.status,
+            gender: character.gender,
+          })),
+        }))
+      );
   }
 }
